ec611bde2309d2b1332db6835227953728649760
ism21766118 <ism21766118@a07.informatica.escoladeltreball.org>
2018-02-01 12:58:07 +0100

    Bucles

--- nveces_01.py
+++ nveces_01.py
@@ -1,7 +1,10 @@
 #coding: utf-8
 x=input("indique su numero aqui:")
+suma=0
 cont=0
 while (cont<x):
-	aux=input("indique un numero:")
+	aux=input("indique su numero aqui:")
 	if (aux>0):
-	    cont+=1
+           cont+=1
+	   suma=suma+aux
+print suma
