a3aac3908f22fc22dea3443df312757e0f69606e
ism21766118 <ism21766118@a07.informatica.escoladeltreball.org>
2018-02-05 13:29:37 +0100

    Bucles

---
+++ mayork07.py
@@ -0,0 +1,10 @@
+#coding: utf-8
+num1=input("introduce un numero entero minimo:")
+num2=input("introduce un numero entero maximo:")
+num3=input("introduce un numero:")
+
+while (num3>num1) and (num3<num2):
+	num3=input("introduce otro numero:")
+	
+print "programa terminado"
+
